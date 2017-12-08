5847f4489d15147399de38a3b6baac211a395e2b
Syed Faraaz Ahmad <faraaz98@live.com>
2017-12-08 19:39:25 +0530

    add proper docs

--- lib/norris.ex
+++ lib/norris.ex
@@ -1,14 +1,20 @@
 defmodule Norris do
   @moduledoc """
-  Documentation for Norris.
+  Documentation for Norris. This is a simple package
+  that gets a chuck norris fact from api.chucknorris.io
   """
 
   @doc """
       Send a request to chucknorris.io API
+      to get a random(uncategorised) fact, 
+      which returns a json string, 
+      then decode the json string into a Map. 
+      Then return the joke string.
   """
   def random do
-    response = "https://api.chucknorris.io/jokes/random"
-    |> Tesla.get()
+    response = 
+      "https://api.chucknorris.io/jokes/random"
+        |> Tesla.get()
 
     response = response.body
     {:ok, struct} = Poison.decode response
